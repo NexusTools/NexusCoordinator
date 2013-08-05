564b0ece1d478742ab0cc58d6919dd8b6a553fa4
Katelyn <ktaeyln@gmail.com>
2013-08-05 18:40:07 -0400

    Compatibility and usability fixes

--- shell/main.cpp
+++ shell/main.cpp
@@ -1,25 +1,43 @@
 #include <unistd.h>
 #include <sys/wait.h>
+#include <string.h>
+#include <stdio.h>
 
 int main(int argc, char *argv[])
 {
-    int fork_rv = fork();
-    if (fork_rv == 0)
-    {
-        execl("/usr/bin/NexusCoordinatorConsole", "NexusCoordinatorConsole", 0);
-        _exit(-1);
-    } else if(fork_rv > 0) {
-        sleep(2);
+    if(argc == 1) {
+        printf("Starting NexusCoordinator ... ");
+        fflush(stdout);
 
-        int status;
-        if(waitpid(fork_rv, &status, WNOHANG) == 0) {
-            while (-1 == waitpid(fork_rv, &status, 0));
-            return status;
+        int fork_rv = fork();
+        if (fork_rv == 0)
+        {
+            execl("/usr/bin/NexusCoordinatorConsole", "NexusCoordinatorConsole", "--shell", 0);
+            _exit(-1);
+        } else if(fork_rv > 0) {
+            sleep(2);
+
+            int status;
+            if(waitpid(fork_rv, &status, WNOHANG) == 0) {
+                while (-1 == waitpid(fork_rv, &status, 0));
+                return status;
+            }
         }
+
+        printf("failed to start.\nDropping to shell...\n\n");
+        fflush(stdout);
     }
 
-    execl("/bin/bash", "bash", 0);
-    execl("/bin/usr/bash", "bash", 0);
-    execl("/bin/sh", "sh", 0);
-    execl("/bin/usr/sh", "sh", 0);
+
+    strcpy(argv[0], "bash");
+    execv("/bin/bash", argv);
+    execv("/bin/usr/bash", argv);
+
+    strcpy(argv[0], "sh");
+    execv("/bin/sh", argv);
+    execv("/bin/usr/sh", argv);
+
+    printf("Neither bash or sh found, exiting...");
+
+    return 1;
 }
